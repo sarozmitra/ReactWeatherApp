46f499f453d7ae45a46a245c18966d3e7da7e136
sarozmitra <sarozmitra@gmail.com>
2017-04-19 12:40:48 +0100

    Fixed search box issues

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,7 +5,14 @@ var Nav = React.createClass({
 
 	onSearch: function(e){
 		e.preventDefault();
-		alert('Not done yet');
+		
+		var location = this.refs.search.value;
+		var encodedLocation = encodeURIComponent(location);
+
+		if(location.length > 0){
+			this.refs.search.value = '';
+			window.location.hash ='#/?location=' + encodedLocation;
+		}
 	},
 
 	render: function(){
@@ -33,7 +40,7 @@ var Nav = React.createClass({
 				<form onSubmit={this.onSearch}> 
 					<ul className="menu">
 						<li>
-							<input type="search" placeholder="Search Weather"/>
+							<input type="search" ref="search" placeholder="Search Weather"/>
 						</li>
 						<li>
 							<input type="submit" className="button" value="Get Weather" />
